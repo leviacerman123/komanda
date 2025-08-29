25a4c420470a7681836b25abf33bfbc19d572012
JASURBE77 <animesila89@gmail.com>
2025-08-29 15:14:13 +0500

    sa

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
                         <div className='flex flex-col gap-5'>
                             <h1 className='text-[#2C2B35] font-semibold text-[60px] w-[646px]'>Управление запасами розничного магазина</h1>
                             <p className='font-normal text-xl text-[#2C2B35] w-[608px]'>Использование сервиса 1С-Товары позволяет значительно упростить работу по формированию заявок поставщику.</p>
-                            <button className='w-[249px] h-[42px] rounded-3xl bg-green-400 text-white'>Заказать демонстрацию</button>
+                            <button className='w-[249px] hover:bg-white hover:border-1 hover:border-black hover:text-black transition-[300ms] h-[42px] rounded-3xl bg-green-400 text-white'>Заказать демонстрацию</button>
                         </div>
                         <img src={rasm} alt="" />
                     </div>
